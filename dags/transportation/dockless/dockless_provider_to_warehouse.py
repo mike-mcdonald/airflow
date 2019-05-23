e93eb9ead3f8ede0e281220c9889279369e98f3e
Michael McDonald <michaelmcdonald@Mikes-MacBook-Pro-2.local>
2019-05-23 10:18:07 -0700

    Update to use new operators

--- dags/transportation/dockless/dockless_provider_to_warehouse.py
+++ dags/transportation/dockless/dockless_provider_to_warehouse.py
@@ -7,32 +7,38 @@ import airflow
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 
-from airflow.operators.mobility_plugin import MobilityTripsToSqlTablesOperator, MobilityEventsToSqlTableOperator
+from airflow.operators.mobility_plugin import (
+    MobilityTripsToSqlExtractOperator,
+    MobilityTripsToSqlWarehouseOperator,
+    MobilityEventsToSqlExtractOperator,
+    MobilityEventsToSqlStageOperator,
+    MobilityEventsToSqlWarehouseOperator
+)
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date':  datetime(2019, 4, 26),
-    'email': ['pbotsqldbas@portlandoregon.gov'],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=2),
-    'concurrency': 50
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date":  datetime(2019, 4, 26),
+    "email": ["pbotsqldbas@portlandoregon.gov"],
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=2),
+    "concurrency": 50
+    # "queue": "bash_queue",
+    # "pool": "backfill",
+    # "priority_weight": 10,
+    # "end_date": datetime(2016, 1, 1),
 }
 
 dag = DAG(
-    dag_id='dockless-provider-to-datalake',
+    dag_id="dockless-provider-to-datalake",
     default_args=default_args,
     catchup=True,
-    schedule_interval='@hourly',
+    schedule_interval="@hourly",
 )
 
-providers = ['lime', 'clevr', 'spin', 'jump', 'razor', 'shared', 'bolt']
+providers = ["lime", "clevr", "spin", "jump", "razor", "shared", "bolt"]
 
 task1 = DummyOperator(
     task_id="provider_extract_start",
@@ -46,7 +52,7 @@ task2 = DummyOperator(
 
 # Extract data from providers and stage in tables
 for provider in providers:
-    trip_extract_task = MobilityTripsToSqlTablesOperator(
+    trip_extract_task = MobilityTripsToSqlExtractOperator(
         task_id=f"loading_{provider}_trips",
         provide_context=True,
         mobility_provider_conn_id=f"mobility_provider_{provider}",
@@ -54,7 +60,7 @@ for provider in providers:
         sql_conn_id="azure_sql_server_default",
         dag=dag)
 
-    event_extract_task = MobilityEventsToSqlTableOperator(
+    event_extract_task = MobilityEventsToSqlExtractOperator(
         task_id=f"loading_{provider}_events",
         provide_context=True,
         mobility_provider_conn_id=f"mobility_provider_{provider}",
@@ -74,11 +80,6 @@ task3 = DummyOperator(
 )
 
 # Run SQL scripts to transform extract data into staged facts
-trips_stage_task = MobilityTripsToSqlStageOperator(
-    task_id=f"staging_trips",
-    provide_context=True,
-    sql_conn_id="azure_sql_server_default",
-    dag=dag)
 
 event_stage_task = MobilityEventsToSqlStageOperator(
     task_id=f"staging_states",
@@ -86,10 +87,13 @@ event_stage_task = MobilityEventsToSqlStageOperator(
     sql_conn_id="azure_sql_server_default",
     dag=dag)
 
-trips_stage_task.set_upstream(task2)
 event_stage_task.set_upstream(task2)
 
-trips_stage_task.set_downstream(task3)
 event_stage_task.set_downstream(task3)
 
 # Run SQL scripts to load staged data to warehouse
+trip_load_task = MobilityTripsToSqlWarehouseOperator(
+    task_id=f"staging_trips",
+    provide_context=True,
+    sql_conn_id="azure_sql_server_default",
+    dag=dag)
