6c81beda4c92e8e43d2af2c9f02709830ca66f5b
Michael McDonald <michaelmcdonald@Mikes-MacBook-Pro-2.local>
2019-05-23 10:15:41 -0700

    Update plugin exports

--- plugins/transportation_plugins/mobility_plugin/__init__.py
+++ plugins/transportation_plugins/mobility_plugin/__init__.py
@@ -2,14 +2,22 @@ from airflow.plugins_manager import AirflowPlugin
 
 from transportation_plugins.mobility_plugin.hooks.mobility_provider_hook import MobilityProviderHook
 from transportation_plugins.mobility_plugin.hooks.areas_of_interest_hook import AreasOfInterestHook
-from transportation_plugins.mobility_plugin.operators.mobility_trips_to_azure_datalake_operator import MobilityTripsToAzureDataLakeOperator
+from transportation_plugins.mobility_plugin.operators.mobility_trips_to_sql_extract_operator import MobilityTripsToSqlExtractOperator
+from transportation_plugins.mobility_plugin.operators.mobility_trips_to_sql_warehouse_operator import MobilityTripsToSqlWarehouseOperator
+from transportation_plugins.mobility_plugin.operators.mobility_events_to_sql_extract_operator import MobilityEventsToSqlExtractOperator
+from transportation_plugins.mobility_plugin.operators.mobility_events_to_sql_stage_operator import MobilityEventsToSqlStageOperator
+from transportation_plugins.mobility_plugin.operators.mobility_routes_to_sql_warehouse_operator import MobilityEventsToSqlWarehouseOperator
 
 # Defining the plugin class
 
 
 class MobilityPlugin(AirflowPlugin):
     name = "mobility_plugin"
-    operators = [MobilityTripsToAzureDataLakeOperator]
+    operators = [MobilityTripsToSqlExtractOperator,
+                 MobilityTripsToSqlWarehouseOperator,
+                 MobilityEventsToSqlExtractOperator,
+                 MobilityEventsToSqlStageOperator,
+                 MobilityEventsToSqlWarehouseOperator]
     hooks = [MobilityProviderHook, AreasOfInterestHook]
     executors = []
     macros = []
