6d73162a4e49651b92822821dc8964d1488491ab
Michael McDonald <michaelmcdonald@Mikes-MacBook-Pro-2.local>
2019-06-10 18:38:57 -0700

    Add better logging

--- plugins/transportation_plugins/mobility_plugin/hooks/mobility_provider_hook.py
+++ plugins/transportation_plugins/mobility_plugin/hooks/mobility_provider_hook.py
@@ -8,6 +8,7 @@ from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 
 from airflow.hooks.base_hook import BaseHook
+from airflow.exceptions import AirflowException
 
 
 class MobilityProviderHook(BaseHook):
@@ -17,9 +18,9 @@ class MobilityProviderHook(BaseHook):
                  mobility_provider_token_conn_id="mobility_provider_token_default"):
         try:
             self.connection = self.get_connection(mobility_provider_conn_id)
-        except:
+        except AirflowException as err:
             self.log.error(
-                f"Failed to find connection for mobility provider: {mobility_provider_conn_id}")
+                f"Failed to find connection for mobility provider: {mobility_provider_conn_id}. Error: {err}")
 
         self.session = requests.Session()
 
@@ -54,7 +55,9 @@ class MobilityProviderHook(BaseHook):
 
         Returns payload(s).
         """
+        self.log.debug(f"Making request to: {url}")
         res = self.session.get(url, params=params)
+        self.log.debug(f"Received response from {url}")
 
         if res.status_code is not 200:
             self.log.warning(res)
@@ -62,15 +65,17 @@ class MobilityProviderHook(BaseHook):
         if "Content-Type" in res.headers:
             cts = res.headers["Content-Type"].split(";")
             if "application/vnd.mds.provider+json" not in cts:
-                self.log.warning(f"Incorrect content-type returned: {res.headers['Content-Type']}")
+                self.log.warning(
+                    f"Incorrect content-type returned: {res.headers['Content-Type']}")
             cts = cts[1:]
             for ct in cts:
                 if ct.strip().startswith("charset"):
                     pass
                 if not ct.strip().startswith("version=0.3"):
-                    self.log.warning(f"Incorrect content-type returned: {res.headers['Content-Type']}")
+                    self.log.warning(
+                        f"Incorrect content-type returned: {res.headers['Content-Type']}")
         else:
-            self.log.warning(f"Missing 0.3 content-type header.")
+            self.log.error(f"Missing 0.3 content-type header.")
 
         page = res.json()
 
@@ -102,15 +107,16 @@ class MobilityProviderHook(BaseHook):
                           Should be a datetime object or numeric representation of UNIX seconds
         """
         # convert datetimes to querystring friendly format
+        self.log.debug(
+            f"Retrieving trips for period {min_end_time} to {max_end_time}")
+
         if min_end_time is not None:
             min_end_time = self._date_format(min_end_time)
         if max_end_time is not None:
             max_end_time = self._date_format(max_end_time)
 
         # gather all the params togethers
-        params = {
-            **dict(device_id=device_id, vehicle_id=vehicle_id, min_end_time=min_end_time, max_end_time=max_end_time),
-        }
+        params = dict(min_end_time=min_end_time, max_end_time=max_end_time)
 
         # make the request(s)
         trips = self._request(
@@ -131,8 +137,9 @@ class MobilityProviderHook(BaseHook):
                           Should be a datetime object or numeric representation of UNIX seconds
 
         """
-        if providers is None:
-            providers = self.providers
+
+        self.log.debug(
+            f"Retrieving events for period {start_time} to {end_time}")
 
         # convert datetimes to querystring friendly format
         if start_time is not None:
@@ -141,9 +148,7 @@ class MobilityProviderHook(BaseHook):
             end_time = self._date_format(end_time)
 
         # gather all the params together
-        params = {
-            **dict(start_time=start_time, end_time=end_time)
-        }
+        params = dict(start_time=start_time, end_time=end_time)
 
         # make the request(s)
         status_changes = self._request(
