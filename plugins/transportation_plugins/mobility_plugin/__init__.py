0d6d19e71c31e23c6b7a101dfd4b188426850f75
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2019-06-28 09:30:29 -0700

    Remove unneeded SQL operator wrappers

--- plugins/transportation_plugins/mobility_plugin/__init__.py
+++ plugins/transportation_plugins/mobility_plugin/__init__.py
@@ -3,9 +3,6 @@ from airflow.plugins_manager import AirflowPlugin
 from transportation_plugins.mobility_plugin.hooks.mobility_provider_hook import MobilityProviderHook
 from transportation_plugins.mobility_plugin.operators.mobility_trips_to_sql_extract_operator import MobilityTripsToSqlExtractOperator
 from transportation_plugins.mobility_plugin.operators.mobility_events_to_sql_extract_operator import MobilityEventsToSqlExtractOperator
-from transportation_plugins.mobility_plugin.operators.mobility_events_to_sql_stage_operator import MobilityEventsToSqlStageOperator
-from transportation_plugins.mobility_plugin.operators.mobility_states_to_sql_warehouse_operator import MobilityStatesToSqlWarehouseOperator
-from transportation_plugins.mobility_plugin.operators.mobility_routes_to_sql_warehouse_operator import MobilityEventsToSqlWarehouseOperator
 from transportation_plugins.mobility_plugin.operators.mobility_provider_sync_operator import MobilityProviderSyncOperator
 from transportation_plugins.mobility_plugin.operators.mobility_vehicle_sync_operator import MobilityVehicleSyncOperator
 from transportation_plugins.mobility_plugin.operators.mobility_fleet_to_sql_extract_operator import MobilityFleetToSqlExtractOperator
@@ -16,8 +13,6 @@ class MobilityPlugin(AirflowPlugin):
     name = "mobility_plugin"
     operators = [MobilityTripsToSqlExtractOperator,
                  MobilityEventsToSqlExtractOperator,
-                 MobilityEventsToSqlStageOperator,
-                 MobilityStatesToSqlWarehouseOperator,
                  MobilityProviderSyncOperator,
                  MobilityVehicleSyncOperator,
                  MobilityFleetToSqlExtractOperator]
