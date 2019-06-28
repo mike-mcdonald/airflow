d02dd7d3283610e55f4722a03ef6dba0dccea2de
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2019-06-28 14:20:52 -0700

    Add processing to map to cities, pattern areas, and parking districts

--- plugins/transportation_plugins/mobility_plugin/operators/mobility_events_to_sql_extract_operator.py
+++ plugins/transportation_plugins/mobility_plugin/operators/mobility_events_to_sql_extract_operator.py
@@ -3,6 +3,7 @@ import json
 import pathlib
 import os
 
+from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime, timedelta
 from tempfile import NamedTemporaryFile
 
@@ -26,7 +27,14 @@ class MobilityEventsToSqlExtractOperator(BaseOperator):
 
     """
 
-    template_fields = ('events_local_path', 'events_remote_path',)
+    template_fields = ('events_local_path',
+                       'events_remote_path',
+                       'cities_local_path',
+                       'cities_remote_path',
+                       'parking_districts_local_path',
+                       'parking_districts_remote_path',
+                       'pattern_areas_local_path',
+                       'pattern_areas_remote_path',)
 
     def __init__(self,
                  mobility_provider_conn_id="mobility_provider_default",
@@ -35,6 +43,12 @@ class MobilityEventsToSqlExtractOperator(BaseOperator):
                  data_lake_conn_id="azure_data_lake_default",
                  events_local_path=None,
                  events_remote_path=None,
+                 cities_local_path=None,
+                 cities_remote_path=None,
+                 parking_districts_local_path=None,
+                 parking_districts_remote_path=None,
+                 pattern_areas_local_path=None,
+                 pattern_areas_remote_path=None,
                  * args, **kwargs):
         super().__init__(*args, **kwargs)
         self.sql_conn_id = sql_conn_id
@@ -43,10 +57,18 @@ class MobilityEventsToSqlExtractOperator(BaseOperator):
         self.data_lake_conn_id = data_lake_conn_id
         self.events_local_path = events_local_path
         self.events_remote_path = events_remote_path
+        self.cities_local_path = cities_local_path
+        self.cities_remote_path = cities_remote_path
+        self.parking_districts_local_path = parking_districts_local_path
+        self.parking_districts_remote_path = parking_districts_remote_path
+        self.pattern_areas_local_path = pattern_areas_local_path
+        self.pattern_areas_remote_path = pattern_areas_remote_path
 
     def execute(self, context):
         end_time = context.get("execution_date")
-        start_time = end_time - timedelta(hours=12)
+        pace = timedelta(hours=2) if datetime.now().date() > context.get(
+            "execution_date").date() else timedelta(hours=12)
+        start_time = end_time - pace
 
         # Create the hook
         hook = MobilityProviderHook(
@@ -61,6 +83,17 @@ class MobilityEventsToSqlExtractOperator(BaseOperator):
             self.log.warning(
                 f"Received no events for time period {start_time} to {end_time}")
             return
+
+        events['event_time'] = events.event_time.map(
+            lambda x: datetime.fromtimestamp(x / 1000).astimezone(timezone("US/Pacific")))
+
+        events['event_hash'] = events.apply(
+            lambda x: hashlib.md5(
+                f"{x.device_id}{x.event_type_reason}{x.event_time.strftime('%d%m%Y%H%M%S%f')}".encode(
+                    'utf-8')
+            ).hexdigest(), axis=1)
+
+        events = events.drop_duplicates(subset='event_hash')
 
         events['batch'] = context.get("ts_nodash")
         events['seen'] = datetime.now()
@@ -77,36 +110,67 @@ class MobilityEventsToSqlExtractOperator(BaseOperator):
 
         events['propulsion_type'] = events.propulsion_type.map(
             lambda x: ','.join(sorted(x)))
-        events['event_time'] = events.event_time.map(
-            lambda x: datetime.fromtimestamp(x / 1000).astimezone(timezone("US/Pacific")))
         events['event_time'] = events.event_time.map(lambda x: x.replace(
             tzinfo=None))  # Remove timezone info after shifting
         events['event_time'] = events.event_time.dt.round('L')
         events['date_key'] = events.event_time.map(
             lambda x: int(x.strftime('%Y%m%d')))
-        events['event_hash'] = events.apply(
-            lambda x: hashlib.md5(
-                f"{x.device_id}{x.event_type_reason}{x.event_time.strftime('%d%m%Y%H%M%S%f')}".encode(
-                    'utf-8')
-            ).hexdigest(), axis=1)
         events['event_time'] = events.event_time.map(
             lambda x: x.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
 
-        events = events.drop_duplicates(subset='event_hash')
-
+        # Aggregate the location to a cell
         hook = AzureMsSqlDataFrameHook(
             azure_mssql_conn_id=self.sql_conn_id)
 
-        # Aggregate the location to a cell
         cells = hook.read_table_dataframe(table_name="cell", schema="dim")
         cells['geometry'] = cells.wkt.map(lambda g: loads(g))
         cells = gpd.GeoDataFrame(cells)
         cells.crs = {'init': 'epsg:4326'}
 
-        events['event_location'] = gpd.sjoin(
+        events['cell_key'] = gpd.sjoin(
             events, cells, how="left", op="within")['key']
 
         del cells
+
+        hook = AzureDataLakeHook(
+            azure_data_lake_conn_id=self.data_lake_conn_id
+        )
+
+        def find_geospatial_dim(local_path, remote_path):
+            pathlib.Path(os.path.dirname(local_path)
+                         ).mkdir(parents=True, exist_ok=True)
+
+            df = hook.download_file(
+                local_path, remote_path)
+            df = gpd.read_file(local_path)
+            df['geometry'] = df.wkt.map(loads)
+            df.crs = {'init': 'epsg:4326'}
+
+            series = gpd.sjoin(
+                events.copy(), df, how="left", op="within")['key']
+
+            del df
+            os.remove(local_path)
+
+            return series
+
+        with ThreadPoolExecutor(max_workers=3) as executor:
+            city_key = executor.submit(find_geospatial_dim,
+                                       self.cities_local_path, self.cities_remote_path)
+            parking_district_key = executor.submit(find_geospatial_dim,
+                                                   self.parking_districts_local_path, self.parking_districts_remote_path)
+            pattern_area_key = executor.submit(find_geospatial_dim,
+                                               self.pattern_areas_local_path, self.pattern_areas_remote_path)
+
+            events['city_key'] = city_key.result()
+            events['parking_district_key'] = parking_district_key.result()
+            events['pattern_area_key'] = pattern_area_key.result()
+
+            del city_key
+            del parking_district_key
+            del pattern_area_key
+
+        del events['event_location']
 
         events = events.rename(index=str, columns={
             'event_type': 'state',
@@ -130,16 +194,15 @@ class MobilityEventsToSqlExtractOperator(BaseOperator):
             'event_time',
             'state',
             'event',
-            'event_location',
+            'cell_key',
+            'city_key',
+            'parking_district_key',
+            'pattern_area_key',
             'battery_pct',
             'associated_trip',
             'seen',
             'batch'
         ]].to_csv(self.events_local_path, index=False)
-
-        hook = AzureDataLakeHook(
-            azure_data_lake_conn_id=self.data_lake_conn_id
-        )
 
         hook.upload_file(self.events_local_path, self.events_remote_path)
         hook.set_expiry(self.events_remote_path, 'RelativeToNow',
