4fc8419216318abb2e91ccf9706227eb355ac2ec
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2020-02-09 20:40:24 -0800

    Add working extract to data lake

--- dags/transportation/dockless/scooter_pilot_1_vehicles_to_warehouse.py
+++ dags/transportation/dockless/scooter_pilot_1_vehicles_to_warehouse.py
@@ -25,10 +25,9 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
-from airflow.hooks.common_plugin import PgSqlDataFrameHook
 from airflow.hooks.azure_plugin import AzureDataLakeHook
 from airflow.hooks.dataframe_plugin import AzureMsSqlDataFrameHook
-from airflow.hooks.mobility_plugin import SharedStreetsAPIHook
+from airflow.hooks.dataframe_plugin import PgSqlDataFrameHook
 
 from airflow.operators.azure_plugin import AzureDataLakeRemoveOperator
 from airflow.operators.mssql_plugin import MsSqlOperator
@@ -53,18 +52,19 @@ default_args = {
 dag = DAG(
     dag_id='scooter_pilot_1_vehicles_to_warehouse',
     default_args=default_args,
+    schedule_interval=None,
     max_active_runs=3,
 )
 
 
-def process_vehicles_to_data_lake(**kwargs):
+def extract_vehicles_to_data_lake(**kwargs):
     # Create the hook
     hook_pgsql = PgSqlDataFrameHook(
-        pgsql_conn_id='pgsql_scooter_pilot_1'
+        pgsql_conn_id=kwargs.get('pgsql_conn_id')
     )
 
     # Get trips as a GeoDataFrame
-    vehicles = hook_pgsql.read_sql('''
+    vehicles = hook_pgsql.read_sql_dataframe('''
         select
             v.name as vehicle_id
         from
@@ -73,6 +73,9 @@ def process_vehicles_to_data_lake(**kwargs):
 
     vehicles['device_id'] = vehicles.apply(lambda x: str(uuid.uuid4()), axis=1)
     vehicles['vehicle_type'] = 'scooter'
+
+    pathlib.Path(os.path.dirname(kwargs.get('templates_dict').get('local_path'))
+                 ).mkdir(parents=True, exist_ok=True)
 
     vehicles.to_csv(kwargs.get('templates_dict').get(
         'local_path'), index=False)
@@ -90,43 +93,44 @@ extract_data_lake_task = PythonOperator(
     task_id='extract_routes_to_data_lake',
     dag=dag,
     provide_context=True,
-    python_callable=extract_shst_hits_datalake,
+    python_callable=extract_vehicles_to_data_lake,
     templates_dict={
         'local_path': '/usr/local/airflow/tmp/{{ ti.dag_id }}/{{ ti.task_id }}/{{ ts_nodash }}.csv',
         'remote_path': '/transportation/mobility/etl/pilot_1/vehicles/{{ ts_nodash }}.csv',
     },
     op_kwargs={
-        'azure_datalake_conn_id': 'azure_data_lake_default'
+        'azure_datalake_conn_id': 'azure_data_lake_default',
+        'pgsql_conn_id': 'pgsql_scooter_pilot_1'
     },
 )
 
-insert_pilot_1_vehicle_task = MsSqlOperator(
-    task_id=f'insert_pilot_1_vehicles',
-    dag=dag,
-    mssql_conn_id='azure_sql_server_full',
-    pool='scooter_azure_sql_server',
-    sql='''
-    insert into
-        dim.vehicle (
-            device_id,
-            vehicle_id,
-            vehicle_type
-        )
-    select distinct
-        device_id,
-        vehicle_id,
-        vehicle_type
-    from
-        etl.external_pilot_1_vehicle as e
-    where
-        not exists (
-            select
-                1
-            from
-                dim.vehicle as v
-            where
-                v.vehicle_id = e.vehicle_id
-        )
-    ''')
+# insert_pilot_1_vehicle_task = MsSqlOperator(
+#     task_id=f'insert_pilot_1_vehicles',
+#     dag=dag,
+#     mssql_conn_id='azure_sql_server_full',
+#     pool='scooter_azure_sql_server',
+#     sql='''
+#     insert into
+#         dim.vehicle (
+#             device_id,
+#             vehicle_id,
+#             vehicle_type
+#         )
+#     select distinct
+#         device_id,
+#         vehicle_id,
+#         vehicle_type
+#     from
+#         etl.external_pilot_1_vehicle as e
+#     where
+#         not exists (
+#             select
+#                 1
+#             from
+#                 dim.vehicle as v
+#             where
+#                 v.vehicle_id = e.vehicle_id
+#         )
+#     ''')
 
-extract_data_lake_task >> insert_pilot_1_vehicle_task
+# extract_data_lake_task >> insert_pilot_1_vehicle_task
